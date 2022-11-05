2e37f0f456a55037533efc7203e2fdfc84003aab
Evi Ofekeze <eviofekeze@u.boisestate.edu>
2022-11-05 08:54:30 -0600

    Updated docker installer

--- docker-install.sh
+++ docker-install.sh
@@ -1,33 +1,78 @@
 #! /bin/bash
 
-#Description: Script to install Docker on Linux, This is compatible with Fedora and Debian Family
+#Description: Script to install Docker on Linux, This is compatible with Fedora, Alpine and Debian Family
 #Author: eviofekeze
 #Date: Nov 4
 
 
-#Remove docker 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update -y
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
 
-if [ $? -ne 0 ]
+echo "Identifying Operating System"
+DISTRO=$(cat /etc/os-release | head -1 | awk -F= '{print $2}' | sed 's/"//g')
+UBT='Ubuntu'
+COS='(Centos.*)'
+RHEL='(Fedora)|(Red Hat.*)'
+ALP='Alpine*'
+
+if [ $DISTRO == $UBT ]
 then
-	sudo chmod a+r /etc/apt/keyrings/docker.gpg
+        echo "Ubuntu Detected"
+	#Remove docker
+	sudo apt-get remove docker docker-engine docker.io containerd runc
 	sudo apt-get update -y
-fi
+	sudo apt-get install ca-certificates curl gnupg lsb-release -y
+	sudo mkdir -p /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	echo \
+	      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update -y
 
-# Install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+	if [ $? -ne 0 ]
+	then
+        	sudo chmod a+r /etc/apt/keyrings/docker.gpg
+        	sudo apt-get update -y
+	fi
 
-#Test 
-sudo docker run hello-world
+	# Install docker
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+	#Test
+	sudo docker run hello-world
+
+elif [ $DISTRO =~ $COS ]
+then
+        echo " OS: $DISTRO Detected"
+	sudo yum remove docker docker-client docker-client-latest docker-common \
+                  docker-latest docker-latest-logrotate docker-logrotate \
+                  docker-engine podman runc -y
+	sudo yum install -y yum-utils
+	sudo yum-config-manager \
+		--add-repo \
+		https://download.docker.com/linux/centos/docker-ce.repo
+	sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+	sudo systemctl start docker
+	sudo docker run hello-world
+elif [ $DISTRO =~ $RHEL ]
+then
+	echo "OS : $DISTRO Dectected"
+	sudo yum remove docker docker-client docker-client-latest docker-common \
+                  docker-latest docker-latest-logrotate docker-logrotate \
+                  docker-engine podman runc -y
+        sudo yum install -y yum-utils
+        sudo yum-config-manager \
+                --add-repo \
+                https://download.docker.com/linux/rhel/docker-ce.repo
+        sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        sudo systemctl start docker
+        sudo docker run hello-world
+elif [ $DISTRO =~ $ALPHINE ]
+then
+        echo " OS: $DISTRO Detected"
+	sudo apk add docker
+	sudo addgroup username docker
+	sudo rc-update add docker boot
+	sudo service docker start
+	sudo apk add docker-compose
+else
+        echo "Not compatible with operating system"
+exit
