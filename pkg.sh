3a3eca0b3492a681a6c9ff2ba5667f8086c93acf
Evi Ofekeze <77086111+eviofekeze@users.noreply.github.com>
2022-11-06 17:29:46 -0700

    added more packages

--- pkg.sh
+++ pkg.sh
@@ -15,16 +15,16 @@ if [[ "$DISTRO" == "$UBT" ]]
 then
         echo "Ubuntu Detected"
 	#Remove docker
-	sudo apt-get install -y git vim net-tools sysstat zip wget finger
+	sudo apt-get install -y git vim net-tools sysstat zip wget curl
 
 elif [[ "$DISTRO" =~ "$COS" ]]
 then
         echo " OS: $DISTRO Detected"
-	sudo yum install -y git vim net-tools sysstat zip wget finger
+	sudo yum install -y git vim net-tools sysstat zip wget curl
 elif [[ "$DISTRO" =~ "$ALP" ]]
 then
         echo " OS: $DISTRO Detected"
-	sudo apk add -y -y git vim net-tools sysstat zip wget finger
+	sudo apk add -y -y git vim net-tools sysstat zip wget curl
 else
         echo "Not compatible with operating system"
 fi
